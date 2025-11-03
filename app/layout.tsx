4dfc284525eebc5948dfdfa598101b245447612a
swargo <sophie.wargo@gmail.com>
2025-11-03 09:54:34 -0800

    add pages to app routing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import Footer from "@/components/Footer";
+import Navigation from "@/components/Navigation";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navigation />
         {children}
+        <Footer />
       </body>
     </html>
   );
