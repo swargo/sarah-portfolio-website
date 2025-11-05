c992853e18136425cce26d8642027470d938b9a4
swargo <sophie.wargo@gmail.com>
2025-11-05 07:51:16 -0800

    add files

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import { Header } from "@/app/_components/Header";
+
+export default function Project() {
+  return (
+    <div>
+      <Header title={"need dynamic header"} subtitle={"dynamic subtitle"} />
+    </div>
+  );
+}
