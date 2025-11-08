8e2ae1bf5797c8dc6c32cea931ad50aa741df0d2
swargo <sophie.wargo@gmail.com>
2025-11-07 18:08:16 -0800

    start mobile nav

--- app/_components/Navigation/index.tsx
+++ app/_components/Navigation/index.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import { Menu } from "lucide-react";
+import { useState } from "react";
 
 export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <nav className="w-full px-2 md:px-[80px] py-3 md:py-8 relative">
-      <ul className="flex">
+    <header className="w-full px-2 md:px-[80px] py-3 md:py-8 relative">
+      <div className="flex">
         <div className="flex-1 md:-mt-4">
           <Link href="/">
             <img
@@ -13,18 +20,41 @@ export default function Navigation() {
             />
           </Link>
         </div>
-        <div className="flex-1 justify-end hidden md:flex">
-          <li className="inline mx-6">
-            <Link href="/">Portfolio</Link>
-          </li>
-          <li className="inline mx-6">
-            <Link href="/about">About</Link>
-          </li>
-          <li className="inline ml-6">
-            <Link href="/contact">Contact</Link>
-          </li>
+        <nav className="flex-1 justify-end hidden md:flex">
+          <ul>
+            <li className="inline mx-6">
+              <Link href="/">Portfolio</Link>
+            </li>
+            <li className="inline mx-6">
+              <Link href="/about">About</Link>
+            </li>
+            <li className="inline ml-6">
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="md:hidden flex-1 justify-end flex items-center">
+          <Menu onClick={handleClick} />
         </div>
-      </ul>
-    </nav>
+      </div>
+      {isOpen && (
+        <nav
+          className="absolute left-0 top-20 w-full border bg-wh text-center py-4 z-100"
+          onClick={handleClick}
+        >
+          <ul>
+            <li className="px-3 py-3">
+              <Link href="/">Portfolio</Link>
+            </li>
+            <li className="px-3 py-3">
+              <Link href="/about">About</Link>
+            </li>
+            <li className="px-3 py-3">
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
   );
 }
